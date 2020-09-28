6f64d4e4845c415051574fdec17e6b91cd64d248
xinyuz1996 <xzhang959@wisc.edu>
2020-09-28 13:13:58 -0400

    Update files

---
+++ code/xinyu_explore.R
@@ -0,0 +1,14 @@
+rm(list=ls())
+
+main_path= "F:/zxy/Project/TAA/TAA_Consulting/"
+setwd(main_path)
+data <- read.table('data/tabular data.txt',sep="\t", header=TRUE)
+str(data)
+
+# define variables
+
+
+
+
+
+
