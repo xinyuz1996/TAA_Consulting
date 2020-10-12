7507536db8d119f33429fa608812d25c40739ff1
xinyuz1996 <xzhang959@wisc.edu>
2020-10-12 12:03:49 -0400

    Update xinyu_explore.R

--- code/xinyu_explore.R
+++ code/xinyu_explore.R
@@ -13,18 +13,16 @@ pMiss <- function(x){sum(is.na(x))/length(x)*100}
 psMiss <- round(apply(data,2,pMiss), 2)
 psMiss <- psMiss[order(psMiss, decreasing = T)]
 missing_percentage <- paste(psMiss,"%", sep="")
-missMat <- cbind(c(1:56),names(psMiss), missing_percentage)
+missMat <- cbind(c(1:56), names(psMiss), missing_percentage)
 knitr::kable(missMat)
 
 # plot missing map
 missmap(data)
 
-
 # missing value imputation
-
 # add na as a new factor level for each categorical variable
-
 # delete variables that have more than 50% missing value
+
 mis50 <- c(1:56)[which(round(apply(data,2,pMiss), 2) > 50)]
 data1 <- data[,-mis50] # reduce 15 variables
 dim(data1)
@@ -41,5 +39,10 @@ plot(a.out)
 plot(density(data1[,8], na.rm = T))
 x <- unlist(as.vector(na.omit(data1[,8])))
 plot(density(x))
-
 density(na.omit(x))
+
+
+
+
+
+
